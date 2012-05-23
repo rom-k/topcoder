1db98b21a0fc9b830ead9769636b5c0d662e3a19
rom_k <rom.cpp@gmail.com>
2012-05-23 20:25:01 +0900

    ijpc retry A

--- ijpc/2012/a/animals.cpp
+++ ijpc/2012/a/animals.cpp
@@ -1,75 +1,74 @@
 #include "animals.h"
 #include <set>
-#include <map>
+#include <vector>
 using namespace std;
 
 #define MAX_N 100000
 int N;
-map<int,int> n[MAX_N];
+struct cnode {
+    int id;
+    int p;
+    int size;
+    set<int> cs;
+};
+vector<cnode> n;
 
-void _remove(int v) {
+int inittree(int id) {
+    n[id].size = 1;
+    for (set<int>::iterator it=n[id].cs.begin();it!=n[id].cs.end();it++) {
+        int cid = *it;
+        n[cid].p = id;
+        n[cid].cs.erase(id);
+        n[id].size += inittree(cid);
+    }
+    return n[id].size;
+}
+
+void update_size_parent(int id, int red) {
+    n[id].size -= red;
+    if (n[id].p < 0) return;
+    update_size_parent(n[id].p, red);
+}
+
+void remove_node(int id) {
+    if (n[id].p > 0)
+        n[n[id].p].cs.erase(id);
+
+    int red = 1;
+    for (set<int>::iterator it=n[id].cs.begin();it!=n[id].cs.end();it++) {
+        int cid = *it;
+        n[cid].p = -1;
+        red += n[cid].size;
+    }
+
+    update_size_parent(n[id].p, red);
+
+    n[id].p = -1;
+    n[id].size = 1;
+    n[id].cs.clear();
 }
 
 void init(int N0, int E[][2]) {
     N = N0;
+    for (int i=0;i<N;i++) {
+        cnode n1;
+        n1.id = i;
+        n1.p = -1;
+        n.push_back(n1);
+    }
     for (int i=0;i<N-1;i++) {
-        int a = E[i][0];
-        int b = E[i][1];
-        n[a].insert(map<int,int>::value_type(b, -1));
-        n[b].insert(map<int,int>::value_type(a, -1));
+        n[E[i][0]].cs.insert(E[i][1]);
+        n[E[i][1]].cs.insert(E[i][0]);
     }
-
-    bool loop_flag = true;
-    while (loop_flag) {
-        loop_flag = false;
-        for (int i=0;i<N;i++) {
-            for (map<int,int>::iterator it=n[i].begin();it!=n[i].end();it++) {
-                int j     = (*it).first;
-                int j_num = (*it).second;
-                if (j_num > 0) continue;
-
-                int sum = 1;
-                map<int,int>::iterator it2;
-                for (it2=n[j].begin();it2!=n[j].end();it2++) {
-                    int k     = (*it2).first;
-                    int k_num = (*it2).second;
-                    if (i == k) continue;
-                    if (k_num < 0) break;
-                    sum += k_num;
-                }
-
-                if (it2==n[j].end()) n[i][j] = sum;
-                else loop_flag = true;
-            }
-        }
-    }
-
-}
-
-void _reduce(int v, int from, int dec) {
-    for (map<int,int>::iterator it=n[v].begin();it!=n[v].end();it++) {
-        if ((*it).first != from) {
-            _reduce((*it).first, v, dec);
-        }
-    }
-    n[v][from] -= dec;
+    inittree(0);
 }
 
 int query(int v) {
-    for (map<int,int>::iterator it=n[v].begin();it!=n[v].end();it++) {
-        int nei = (*it).first;
-        _reduce(nei, v, n[nei][v]);
-        n[v].erase(nei);
-        n[nei].erase(v);
-    }
-
+    remove_node(v);
     int ret = 0;
-    for (int i=0;i<N;i++) {
-        int ret1 = 1;
-        for (map<int,int>::iterator it=n[i].begin();it!=n[i].end();it++) {
-            ret1 += (*it).second;
-        }
-        ret = max(ret, ret1);
-    }
+    for (int i=0;i<N;i++)
+        if (n[i].p < 0)
+            ret = max(ret, n[i].size);
     return ret;
 }
+
