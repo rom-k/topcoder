a5367aef3fe7f59047fc7b682f87d3c612853d32
rom_k <rom.cpp@gmail.com>
2012-05-29 23:36:02 +0900

    SRM544

---
+++ source/ElectionFraudDiv1.cpp
@@ -0,0 +1,129 @@
+#include <stdio.h>
+#include <time.h>
+#include <vector>
+#include <list>
+#include <set>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <cmath>
+#include <stack>
+#include <numeric>
+#include <iterator>
+#include <cstdio>
+#include <cstdlib>
+#include <climits>
+#include <cfloat>
+#include <utility>
+#include <memory>
+#include <functional>
+#include <complex>
+
+using namespace std;
+
+typedef long long LL;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<double> VD;
+typedef vector<LL> VL;
+
+#define FORE(it, c, T) for(T::iterator it = c.begin(); it != c.end(); it++)
+#define FORI(i, n) for(int i = 0; i < (n); i++)
+#define FORIS(i, s, n) for(int i = (s); i < (n); i++)
+#define CLEAR(a) memset(a, 0, sizeof(a))
+#define SORT(a) sort(a.begin(), a.end())
+#define REVERSE(a) reverse(a.begin(), a.end())
+#define PB(n) push_back(n)
+#define SZ(a) int((a).size())
+#define IPOW(a,b) ((long long) pow((double)(a),(double)(b)))
+#define PI M_PI
+#define EPS 1e-13
+#define INF 0x7f7f7f7f
+
+#define DEBUG 1
+#define DBG(a) if (DEBUG) cout <<"DEBUG::: " <<a <<endl;
+#define DBG2(a, b) if (DEBUG) cout <<"DEBUG::: " <<a <<"=" <<b <<endl;
+#define DBG3(a, b, c) if (DEBUG) cout <<"DEBUG::: " <<a <<":" <<b <<":" <<c <<endl;
+
+class ElectionFraudDiv1 {
+public:
+    int MinimumVoters(VI p) {
+        int n = SZ(p);
+        int sum = 0;
+        SORT(p);
+        FORI(i,n) sum += p[i];
+
+        if (100. < (double)sum - 0.5 * n
+        or  (double)sum + 0.5 * n <= 100.) {
+            return -1;
+        }
+
+        int cnt = 1;
+        int ret;
+        set<int> s;
+        FORI(i,n) s.insert(p[i]);
+        while (1) {
+            bool ok = true;
+            set<int>::iterator it = s.begin();
+            int div = *it;
+            while(div == 0) {it++; div = *it;}
+            FORI(i,n) {
+                int q = p[i] / div;
+                int r = p[i] % div;
+                if (r-1 < -0.5*cnt*(q+1)
+                or  0.5*cnt*q < r) {
+                    ok = false;
+                    break;
+                }
+            }
+            if (ok) {
+                ret = (int)(sum/div + 0.5);
+                bool ok1 = true;
+                int sum_u = 0;
+                FORI(i,n) {
+                    int u = (int)((double)ret/sum*p[i] + 0.5);
+                    sum_u += u;
+                    if (p[i] != (int)((double)u/ret*100 + 0.5)) ok1 = false;
+                }
+                if (ret != sum_u) ok1 = false;
+                if (ok1) break;
+            }
+            set<int> s1;
+            FORE(it,s,set<int>) FORE(it1,s,set<int>)
+                if (*it < *it1) s1.insert(*it1 - *it);
+            s.swap(s1);
+            cnt++;
+        }
+
+        return ret;
+    }
+
+    
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); if ((Case == -1) || (Case == 4)) test_case_4(); if ((Case == -1) || (Case == 5)) test_case_5(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const int &Expected, const int &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { int Arr0[] = {33, 33, 33}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 3; verify_case(0, Arg1, MinimumVoters(Arg0)); }
+	void test_case_1() { int Arr0[] = {29, 29, 43}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 7; verify_case(1, Arg1, MinimumVoters(Arg0)); }
+	void test_case_2() { int Arr0[] = {12, 12, 12, 12, 12, 12, 12, 12}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = -1; verify_case(2, Arg1, MinimumVoters(Arg0)); }
+	void test_case_3() { int Arr0[] = {13, 13, 13, 13, 13, 13, 13, 13}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 8; verify_case(3, Arg1, MinimumVoters(Arg0)); }
+	void test_case_4() { int Arr0[] = {0, 1, 100}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 200; verify_case(4, Arg1, MinimumVoters(Arg0)); }
+	void test_case_5() { int Arr0[] = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9, 3, 2, 3, 8, 4}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 97; verify_case(5, Arg1, MinimumVoters(Arg0)); }
+
+// END CUT HERE
+
+};
+
+// BEGIN CUT HERE
+int main() {
+    ElectionFraudDiv1 ___test;
+    ___test.run_test(-1);
+}
+// END CUT HERE
