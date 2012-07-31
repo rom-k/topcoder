372694e9e50c0108a6ee38a069f01b94ce17b841
rom_k <rom.cpp@gmail.com>
2012-07-31 23:16:57 +0900

    cf 131

--- codeforces/131div2/e_ng.cpp
+++ codeforces/131div2/e_ng.cpp
@@ -4,52 +4,58 @@ typedef long long LL;
 
 #define REPS(i,s,n) for(int i=s;i<n;i++)
 #define REP(i,n) REPS(i,0,n)
-#define max_n 305
-
-int n;
-int a[max_n][max_n];
-LL sum[max_n][max_n];
-bool fromx[max_n][max_n];
 
 int main() {
+    int n;
     cin >>n;
+    int a[n][n];
     REP(i,n)REP(j,n) cin >>a[i][j];
 
+    if (n<3) {
+        int sum = 0;
+        REP(i,n)REP(j,n) sum+=a[i][j];
+        cout <<sum <<endl;
+        return 0;
+    }
+
+    LL sum[n][n];
+    bool fromi[n][n];
     REP(i,n)REP(j,n) sum[i][j]=0;
-    REP(i,n)REP(j,n) fromx[i][j]=false;
+    REP(i,n)REP(j,n) fromi[i][j]=false;
     REP(i,n)REP(j,n) {
-        int costx, costy;
-        costy = (0<=i-1 and i-1<n) ? sum[i-1][j] : -1005;
-        costx = (0<=j-1 and j-1<n) ? sum[i][j-1] : -1005;
-        if (i==0 and j==0) costy = 0;
-        sum[i][j] = max(costx,costy) + a[i][j];
-        if (costx<costy) fromx[i][j]=true;
+        int costi = (0<=i-1 and i-1<n) ? sum[i-1][j] : -1005;
+        int costj = (0<=j-1 and j-1<n) ? sum[i][j-1] : -1005;
+        if (i==0 and j==0) costj = 0;
+        sum[i][j] = max(costi,costj) + a[i][j];
+        if (costi>costj) fromi[i][j]=true;
     }
     int cost = sum[n-1][n-1];
+REP(i,n){ REP(j,n) cout <<" " <<a[i][j]; cout <<endl; }
+cout <<"cost=" <<cost <<endl;
 
-    int x, y;
-    x = y = n-1;
-    while(0<=x and 0<=y) {
-        if (fromx[x][y]) {
-            REP(i,y-1) a[x-1][y]=a[x][y-1];
-            x--;
+    int a2[n][n];
+    REP(i,n)REP(j,n) a2[i][j]=0;
+    REPS(i,1,n)REP(j,n-1) a2[i][j]=a[i][j];
+    int ii, jj;
+    ii = jj = n-1;
+    while(0<=ii and 0<=jj) {
+        if (!fromi[ii][jj]) {
+            REP(i,ii) a2[i+1][jj-1]=a[i][jj];
         }
-        else {
-            y--;
-        }
+        if (fromi[ii][jj]) ii--;
+        else               jj--;
     }
+REP(i,n){ REP(j,n) cout <<" " <<a2[i][j]; cout <<endl; }
 
     REP(i,n)REP(j,n) sum[i][j]=0;
-    REP(i,n)REP(j,n) fromx[i][j]=false;
-    REP(i,n-1)REPS(j,1,n) {
-        int costx, costy;
-        costy = (0<=i-1 and i-1<n) ? sum[i-1][j] : -1005;
-        costx = (0<=j-1 and j-1<n) ? sum[i][j-1] : -1005;
-        if (i==0 and j==1) costy = 0;
-        sum[i][j] = max(costx,costy) + a[i][j];
-        if (costx<costy) fromx[i][j]=true;
+    REPS(i,1,n)REP(j,n-1) {
+        int costi = (0<=i-1 and i-1<n) ? sum[i-1][j] : -1005;
+        int costj = (0<=j-1 and j-1<n) ? sum[i][j-1] : -1005;
+        if (i==1 and j==0) costj = 0;
+        sum[i][j] = max(costi,costj) + a2[i][j];
     }
-    cost += sum[n-2][n-1];
+    cost += sum[n-1][n-2];
+REP(i,n){ REP(j,n) cout <<" " <<sum[i][j]; cout <<endl; }
 
     cout <<cost <<endl;
     return 0;
