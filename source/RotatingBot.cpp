7bf3d20ae5aa330a24eff34d0e5492296905d895
rom_k <rom.cpp@gmail.com>
2012-08-02 00:30:17 +0900

    tc srm 500 div2

---
+++ source/RotatingBot.cpp
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include <time.h>
+#include <vector>
+#include <list>
+#include <set>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <cmath>
+#include <stack>
+#include <numeric>
+#include <iterator>
+#include <cstdio>
+#include <cstdlib>
+#include <climits>
+#include <cfloat>
+#include <utility>
+#include <memory>
+#include <functional>
+#include <complex>
+
+using namespace std;
+
+typedef long long LL;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<double> VD;
+typedef vector<LL> VL;
+typedef pair<int,int> PII;
+typedef pair<LL,LL> PLL;
+
+#define REPE(it, c, T) for(T::iterator it = c.begin(); it != c.end(); it++)
+#define REP(i, n) for(int i = 0; i < (n); i++)
+#define REPS(i, s, n) for(int i = (s); i < (n); i++)
+#define CLEAR(a) memset(a, 0, sizeof(a))
+#define SORT(a) sort(a.begin(), a.end())
+#define REVERSE(a) reverse(a.begin(), a.end())
+#define PB(n) push_back(n)
+#define SZ(a) int((a).size())
+#define IPOW(a,b) ((long long) pow((double)(a),(double)(b)))
+#define PI M_PI
+#define EPS 1e-13
+#define INF 0x7f7f7f7f
+
+#define DEBUG 1
+#define DBG(a) if (DEBUG) cout <<"DEBUG::: " <<a <<endl;
+#define DBG2(a, b) if (DEBUG) cout <<"DEBUG::: " <<a <<"=" <<b <<endl;
+#define DBG3(a, b, c) if (DEBUG) cout <<"DEBUG::: " <<a <<":" <<b <<":" <<c <<endl;
+
+class RotatingBot {
+public:
+    int minArea(vector <int> m) {
+        int n = SZ(m);
+        if (n==1) return m[0]+1;
+        if (n==2) return (m[0]+1)*(m[1]+1);
+        if (n==3) return (max(m[0],m[2])+1)*(m[1]+1);
+        int i, wh[2], ret;
+        if (m[0]==m[2]) {
+            i=3;
+            wh[0]=m[0];
+            wh[1]=m[1]-1;
+            ret=(m[0]+1)*(m[1]+1);
+        }
+        else if (m[0]<m[2] and m[1]==m[3]) {
+            i=4;
+            wh[0]=m[2]-m[0]-1;
+            wh[1]=m[3];
+            ret=(m[2]+1)*(m[1]+1);
+        }
+        else if (m[0]<m[2] and m[1]<m[3]) {
+            i=4;
+            wh[0]=m[2];
+            wh[1]=m[3]-m[1];
+            ret=(m[2]+1)*(m[3]+1);
+        }
+        else if (m[0]<m[2] and m[1]>m[3]) {
+            i=n;
+            if (n==4) ret=(m[2]+1)*(m[1]+1);
+            else      ret=-1;
+        }
+        else {
+            i=n;
+            ret=-1;
+        }
+        while(i<n) {
+            if (i+1< n and wh[i%2]!=m[i]) ret=-1;
+            if (i+1==n and wh[i%2]<m[i]) ret=-1;
+            wh[(i+1)%2]--;
+            i++;
+        }
+        return ret;
+    }
+
+    
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); if ((Case == -1) || (Case == 4)) test_case_4(); if ((Case == -1) || (Case == 5)) test_case_5(); if ((Case == -1) || (Case == 6)) test_case_6(); if ((Case == -1) || (Case == 7)) test_case_7(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const int &Expected, const int &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { int Arr0[] = {15}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 16; verify_case(0, Arg1, minArea(Arg0)); }
+	void test_case_1() { int Arr0[] = {3,10}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 44; verify_case(1, Arg1, minArea(Arg0)); }
+	void test_case_2() { int Arr0[] = {1,1,1,1}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = -1; verify_case(2, Arg1, minArea(Arg0)); }
+	void test_case_3() { int Arr0[] = {9,5,11,10,11,4,10}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 132; verify_case(3, Arg1, minArea(Arg0)); }
+	void test_case_4() { int Arr0[] = {12,1,27,14,27,12,26,11,25,10,24,9,23,8,22,7,21,6,20,5,19,4,18,3,17,2,16,1,15}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 420; verify_case(4, Arg1, minArea(Arg0)); }
+	void test_case_5() { int Arr0[] = {8,6,6,1}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = -1; verify_case(5, Arg1, minArea(Arg0)); }
+	void test_case_6() { int Arr0[] = {8,6,6}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 63; verify_case(6, Arg1, minArea(Arg0)); }
+	void test_case_7() { int Arr0[] = {5,4,5,3,3}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 30; verify_case(7, Arg1, minArea(Arg0)); }
+
+// END CUT HERE
+
+};
+
+// BEGIN CUT HERE
+int main() {
+    RotatingBot ___test;
+    ___test.run_test(-1);
+}
+// END CUT HERE
