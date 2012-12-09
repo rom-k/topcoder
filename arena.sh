2fdc89ba0bf55389faa9437b4d1e4889b8e8bb8c
rom_k <rom.cpp@gmail.com>
2012-12-09 11:21:57 +0900

    update arena.sh

--- arena.sh
+++ arena.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+if [ -L ~/contestapplet.conf ]; then rm ~/contestapplet.conf; fi
+if [ -L ~/contestapplet.conf.bak ]; then rm ~/contestapplet.conf.bak; fi
 ln -s ~/topcoder/conf/contestapplet.conf     ~/contestapplet.conf
 ln -s ~/topcoder/conf/contestapplet.conf.bak ~/contestapplet.conf.bak
 
+#sudo alternatives --set java /usr/lib/jvm/jre-1.7.0-openjdk.x86_64/bin/java
+
 #/usr/lib/jvm/java-6-openjdk/jre/bin/javaws.real /home/kondo/topcoder/ContestAppletProd.jnlp
 #/usr/lib/jvm/java-6-openjdk/jre/bin/javaws.real -J-Xmx128m -Xnofork /home/kondo/topcoder/conf/ContestAppletProd.jnlp
-/usr/lib/jvm/java-6-openjdk/jre/bin/javaws -J-Xmx128m -Xnofork /home/kondo/topcoder/conf/ContestAppletProd.jnlp
+#/usr/lib/jvm/java-6-openjdk/jre/bin/javaws -J-Xmx128m -Xnofork /home/kondo/topcoder/conf/ContestAppletProd.jnlp
+#/usr/lib/jvm/java-1.6.0-ibm-1.6.0.12.0.x86_64/jre/bin/javaws -J-Xmx128m -Xnofork /home/kondo/topcoder/conf/ContestAppletProd.jnlp
+/usr/java/jre1.7.0_09/bin/javaws -J-Xmx128m -Xnofork /home/kondo/topcoder/conf/ContestAppletProd.jnlp
 
-rm ~/contestapplet.conf
-rm ~/contestapplet.conf.bak
+#sudo alternatives --auto java
 
