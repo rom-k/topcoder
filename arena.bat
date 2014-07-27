eaad587f51119451e101138f2c7aebd86809ef7b
rom_k <rom.cpp@gmail.com>
2014-07-27 19:29:42 +0900

    Update arena launch bat

--- arena.bat
+++ arena.bat
@@ -1,2 +1,19 @@
 @echo off
+
+cd /d %~dp0
+
+if exist %USERPROFILE%\contestapplet.conf (
+    del %USERPROFILE%\contestapplet.conf
+)
+if exist %USERPROFILE%\contestapplet.conf.bak (
+    del %USERPROFILE%\contestapplet.conf.bak
+)
+
+if exist conf\contestapplet.conf (
+    copy conf\contestapplet.conf %USERPROFILE%\contestapplet.conf >nul
+)
+if exist conf\contestapplet.conf.bak (
+    copy conf\contestapplet.conf.bak %USERPROFILE%\contestapplet.conf.bak >nul
+)
+
 conf\ContestAppletProd.jnlp
