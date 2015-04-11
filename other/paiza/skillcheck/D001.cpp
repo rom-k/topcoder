57aa6286079b881b6312db1ec278bf35779d88bf
rom <rom.cpp@gmail.com>
2015-04-11 11:08:34 +0900

    .

---
+++ other/paiza/skillcheck/D001.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+	string s1, s2;
+	cin >> s1 >> s2;
+		
+	if (s1 == s2) {
+		cout << "True" << endl;
+	}
+	else {
+		cout << "False" << endl;
+	}
+	
+	return 0;
+}
